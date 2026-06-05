921eec84f2aa5dc1515263e0d3e3f4fe15d79b14
Vinicius Gustavo <vinicius.barboza@ufrpe.br>
2026-06-05 16:45:33 -0300

    feat: add color analyzer and feature extractor

---
+++ color_analyzer.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+
+class ColorAnalyzer:
+
+    @staticmethod
+    def rgb_histogram(image):
+        hist = []
+        for i in range(3):
+            h = cv2.calcHist([image], [i], None, [256], [0,256])
+            hist.append(h.flatten())
+        return hist
+
+    @staticmethod
+    def hsv_histogram(image):
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        hist = []
+        for i in range(3):
+            h = cv2.calcHist([hsv], [i], None, [256], [0,256])
+            hist.append(h.flatten())
+        return hist
+
+    @staticmethod
+    def detect_color_anomaly(histograms):
+        variances = []
+        for hist in histograms:
+            mean = np.mean(hist)
+            var = np.mean((hist - mean) ** 2)
+            variances.append(var)
+
+        return np.mean(variances)
